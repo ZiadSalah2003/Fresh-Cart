1f4150cb95bf2424a1a70eb6e45e28e88458cf85
ZiadSalah2003 <ziadsalah2003@gmail.com>
2025-07-13 03:04:19 +0300

    feat: Integrate cart item count in navigation and update cart service for reactive updates

--- src/app/components/nav-blank/nav-blank.component.ts
+++ src/app/components/nav-blank/nav-blank.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MytranslateService } from '../../core/services/mytranslate.service';
+import { CartService } from '../../core/services/cart.service';
 
 @Component({
   selector: 'app-nav-blank',
@@ -11,12 +12,23 @@ import { MytranslateService } from '../../core/services/mytranslate.service';
   templateUrl: './nav-blank.component.html',
   styleUrl: './nav-blank.component.scss'
 })
-export class NavBlankComponent {
-
+export class NavBlankComponent implements OnInit {
   readonly _authService = inject(AuthService);
   readonly _translateService = inject(TranslateService);
   private readonly _mytranslateService = inject(MytranslateService);
+  private readonly _cartService = inject(CartService);
+  countNumber:number = 0;
 
+   ngOnInit(): void {
+    this._cartService.cartNumber.subscribe({
+      next: (count) => {
+        this.countNumber = count;
+      },
+      error: (err) => {
+        console.error('Error fetching cart number:', err);
+      }
+    });
+  }
   changeLanguage(lang: string):void {
     this._mytranslateService.changeLang(lang);
   }
--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 
 @Injectable({
@@ -9,6 +9,7 @@ import { environment } from '../environments/environment';
 export class CartService {
 
   private readonly _httpClient = inject(HttpClient);
+  cartNumber: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   addProductToCart(id: string): Observable<any> {
     return this._httpClient.post(`${environment.baseUrl}/api/v1/cart`,
        {
